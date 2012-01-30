68e667e867bfb4343439e0e6883ba88a75178ae1
ARJANEN Loïc Jean David <arjanen.loic@gmail.com>
2012-01-30 22:28:11 +0900

    Addition of JSON-RPC v1 support.
    Ignore-this: 57727935276667f03206b1ced58a48ae
    
    Addition of JSON-RPC v1 support, the default version of the protocol is
    version 2. I'll later try to simplify the implementation. I might later add
    server-side and other transports support.
    
    darcs-hash:ec2ea00eeab1dcda65994dc208d4f7cb30186c02

--- Network/JsonRpc/Client.hs
+++ Network/JsonRpc/Client.hs
@@ -6,51 +6,91 @@ module Network.JsonRpc.Client (
 --      notify,
 --      notifyWithVersion,
       JsonRpcCall,
+      JsonRpcVersion(..),
 --      JsonRpcNotice
 ) where
 
 import Network.JsonRpc.Common
 import Data.Aeson
 import Network.HTTP
+import Network.Stream as NS
 import Network.URI
 import Control.Exception
 import Control.Monad
 import Data.Maybe
 import qualified Data.ByteString.Lazy as BS
 
-remote :: (ToJSON a, JsonRpcCall b) => String -> String -> (a -> b)
-remote = remoteWithVersion
+data JsonRpcVersion = Version1 | Version2
 
-remoteWithVersion :: (ToJSON a, JsonRpcCall b) => String -> String -> (a -> b)
-remoteWithVersion url nom = (\x -> marshalAndCall url nom (\xs -> (toJSON x):xs))
+remote :: (ToJSON a, JsonRpcCall b) => String -> String -> (a -> b)
+remote = remoteWithVersion Version2
+
+remoteWithVersion :: (ToJSON a, JsonRpcCall b) => JsonRpcVersion -> String -> String -> (a -> b)
+remoteWithVersion version url nom = (\x -> marshalAndCall version url nom (\xs -> (toJSON x):xs))
 
 class JsonRpcCall a where
-  marshalAndCall :: String -> String -> ([Value] -> [Value]) -> a
+  marshalAndCall :: JsonRpcVersion -> String -> String -> ([Value] -> [Value]) -> a
 
 instance (ToJSON a, JsonRpcCall b) => JsonRpcCall (a -> b) where
-  marshalAndCall url nom f = (\x -> marshalAndCall url nom (\xs -> f ((toJSON x):xs)))
+  marshalAndCall version url nom f = (\x -> marshalAndCall version url nom (\xs -> f ((toJSON x):xs)))
 
-instance JsonRpcCall (IO String) where
-  marshalAndCall url nom f = call url (mkJsonRpcRequest nom (f []))
+instance (FromJSON a) => JsonRpcCall (IO a) where
+  marshalAndCall version url nom f =
+    case version of
+         Version1 -> callv1 url (mkJsonRpcRequest nom (f []))
+         Version2 -> call url (mkJsonRpcRequest nom (f []))
 
-call :: String -> JsonRpcRequest -> IO String
-call url request = 
-  let uri = fromMaybe (error "Invalid URI") (parseURI url)
-      js = encode request
-      headers = [Header HdrUserAgent user_agent, Header HdrContentType "application/json", Header HdrContentLength (show (BS.length js))]
-      req = Request uri POST headers js
-      req_id = getId request
+call :: (FromJSON a) => String -> Version2Request -> IO a
+call url request =
+  let req_id = getId request
+      payload = encode request
   in do
-      res <- simpleHTTP req
-      body <- getResponseBody res
-      return (show body)
-      {- let response = fromMaybe (error "Invalid server response") (decode' body)
-      if (getId response == req_id) then
+       res <- sendPost url payload
+       response <- getResponseBody res
+       processResponse response req_id True
+
+callv1 :: (FromJSON a) => String -> Version1Request -> IO a
+callv1 url request =
+  let req_id = getId request
+      payload = encode request
+  in do
+       res <- sendPost url payload
+       response <- getResponseBody res
+       processResponse1 response req_id False
+
+sendPost :: String -> BS.ByteString -> IO (NS.Result (Response BS.ByteString))
+sendPost url payload =
+  let uri = fromMaybe (error "Invalid URI") (parseURI url)
+      headers = [Header HdrUserAgent user_agent, Header HdrContentType "application/json", Header HdrContentLength (show (BS.length payload))]
+      req = Request uri POST headers payload
+  in simpleHTTP req
+
+processResponse :: (FromJSON a) => BS.ByteString -> Value -> Bool ->  IO a
+processResponse resp req_id is_v2 =
+  let response = fromMaybe (error "Invalid server response") (decode' resp) :: Version2Response
+      resp_id = getId response
+      resp_id_err = is_v2 && resp_id == Null
+  in if (resp_id == req_id || resp_id_err) then
         do
-          case (getAnswer response) of
+          case (getReturnValue response) of
               Left _ -> error "JSON-RPC error"
               Right val -> case (fromJSON val) of
                                 Error _ -> error "Type mismatch"
                                 Success a -> return a
         else
-        error "Bad Id" -}
+          error "Bad Id"
+
+processResponse1 :: (FromJSON a) => BS.ByteString -> Value -> Bool ->  IO a
+processResponse1 resp req_id is_v2 =
+  let response = fromMaybe (error "Invalid server response") (decode' resp) :: Version1Response
+      resp_id = getId response
+      resp_id_err = is_v2 && resp_id == Null
+  in if (resp_id == req_id || resp_id_err) then
+        do
+          case (getReturnValue response) of
+              Left _ -> error "JSON-RPC error"
+              Right val -> case (fromJSON val) of
+                                Error _ -> error "Type mismatch"
+                                Success a -> return a
+        else
+          error "Bad Id"
--- Network/JsonRpc/Common.hs
+++ Network/JsonRpc/Common.hs
@@ -2,12 +2,13 @@
 
 module Network.JsonRpc.Common (
       user_agent,
-      JsonRpcRequest,
-      JsonRpcResponse,
-      mkJsonRpcRequest,
-      JsonRpcMessage,
-      getAnswer,
-      getId
+      Version2Request,
+      Version2Response,
+      Version1Request,
+      Version1Response,
+      JsonRpcMessage(..),
+      JsonRpcRequest(..),
+      JsonRpcResponse(..)
 ) where
 
 import Data.Maybe
@@ -23,24 +24,39 @@ user_agent = "Haskell JSON-RPC Client/0.0"
 default_version :: String
 default_version = "2.0"
 
+class JsonRpcMessage a where
+  getId :: a -> Value
+
+class (JsonRpcMessage a) => JsonRpcResponse a where
+  getReturnValue :: a -> Either JsonRpcError Value
+
+class (JsonRpcMessage a) => JsonRpcRequest a where
+  mkJsonRpcRequest :: String -> [Value] -> a
+
 data JsonRpcError = JsonRpcError Int String
 
 instance FromJSON JsonRpcError where
   parseJSON (Object o) = JsonRpcError <$> o .: "code" <*> o .: "message"
   parseJSON _ = mzero
 
-data JsonRpcRequest = Request Value String [Value]
+data Version2Request = Request Value String [Value]
 
-instance ToJSON JsonRpcRequest where
+instance ToJSON Version2Request where
   toJSON (Request iden meth params) =
     let mandatory = ["jsonrpc" .= default_version, "id" .= iden, "method" .= meth]
     in if (null params)
           then object mandatory
           else object (mandatory ++ ["params" .= params])
 
-data JsonRpcResponse = Response Value (Either JsonRpcError Value)
+instance JsonRpcMessage Version2Request where
+  getId (Request iden _ _) = iden
 
-instance FromJSON JsonRpcResponse where
+instance JsonRpcRequest Version2Request where
+  mkJsonRpcRequest meth params = Request "hs-json-rpc" meth params
+
+data Version2Response = Response Value (Either JsonRpcError Value)
+
+instance FromJSON Version2Response where
   parseJSON (Object o) = do
     v <- (o .: "jsonrpc") :: Parser String
     if (v == "2.0") then
@@ -50,21 +66,42 @@ instance FromJSON JsonRpcResponse where
           a <- maybe (liftM Left (o .: "error")) (return . Right) temp_a
           return (Response i a)
         else
-          mzero
-  parseJSON _ = mzero
---  parseJSON _ = Response Null (Left (JsonRpc2Response -32603 "Internal error"))
+          return (Response Null (Left (JsonRpcError (-32600) "Invalid JSON-RPC")))
+  parseJSON _ = return (Response Null (Left (JsonRpcError (-32600) "Invalid JSON-RPC")))
 
-class JsonRpcMessage a where
-  getId :: a -> Value
-
-instance JsonRpcMessage JsonRpcRequest where
-  getId (Request iden _ _) = iden
-
-instance JsonRpcMessage JsonRpcResponse where
+instance JsonRpcMessage Version2Response where
   getId (Response iden _) = iden
 
-mkJsonRpcRequest :: String -> [Value] -> JsonRpcRequest
-mkJsonRpcRequest meth params = Request "hs-json-rpc" meth params
+instance JsonRpcResponse Version2Response where
+  getReturnValue (Response _ resp) = resp
 
-getAnswer :: JsonRpcResponse -> Either JsonRpcError Value
-getAnswer (Response _ resp) = resp
+data Version1Request = Request1 Value String [Value]
+
+instance ToJSON Version1Request where
+  toJSON (Request1 iden meth params) = object ["id" .= iden, "method" .= meth, "params" .= params]
+
+instance JsonRpcMessage Version1Request where
+  getId (Request1 iden _ _) = iden
+
+instance JsonRpcRequest Version1Request where
+  mkJsonRpcRequest meth params = Request1 "hs-json-rpc" meth params
+
+data Version1Response = Response1 Value (Either JsonRpcError Value)
+
+instance FromJSON Version1Response where
+  parseJSON (Object o) = do
+   i <- o .: "id"
+   a <- o .: "result"
+   if (a == Null)
+     then
+       do
+         e <- o .: "error"
+         return (Response1 i (Left e))
+     else return (Response1 i (Right a))
+  parseJSON _ = return (Response1 Null (Left (JsonRpcError (-32600) "Invalid JSON-RPC")))
+
+instance JsonRpcMessage Version1Response where
+  getId (Response1 iden _) = iden
+
+instance JsonRpcResponse Version1Response where
+  getReturnValue (Response1 _ resp) = resp
